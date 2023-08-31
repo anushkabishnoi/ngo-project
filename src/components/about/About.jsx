630e4d08483b62b56d410f241519acc377172437
anushkabishnoi <anushkabishnoi9@gmail.com>
2023-08-31 22:06:19 +0530

    about hero done :)

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./about.css";
-import Back from "../common/back/Back";
 import AboutCard from "./AboutCard";
 
 const About = () => {
   return (
     <>
-      <Back title="About Us" />
+      <section className="centre">
+        <h1>"Uniting NGOs, Amplifying Impact: Where Collaboration Ignites Change."</h1>
+      </section>
+      <div className="margin"></div>
       <AboutCard />
     </>
   );
