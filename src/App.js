770284e1c69db69930ffd66f92e3444b1c98b3f6
Pragya Shah <pragyashah27@gmail.com>
2023-09-03 10:15:43 +0530

    contact page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/common/header/Header"
 import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import Home from "./components/home/Home"
 import About from "./components/about/About"
-
+import Contact from "./components/contact/Contact"
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/about' component={About} />
+          <Route exact path='/contact' component={Contact} />
         </Switch>
       </Router>
     </>
