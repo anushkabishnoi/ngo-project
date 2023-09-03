1f6232e4d383b03b99c97816ee81b89c011f4e53
anushkabishnoi <anushkabishnoi9@gmail.com>
2023-09-03 10:06:15 +0530

    slider arrow color update

--- src/components/common/slider/Slider.jsx
+++ src/components/common/slider/Slider.jsx
@@ -13,30 +13,34 @@ const Container = styled.div`
 `;
 
 const Arrow = styled.div`
-  width: 50px;
-  height: 50px;
-  background: #fff7f7;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: ${(props) => props.direction === "left" && "10px"};
-  right: ${(props) => props.direction === "right" && "10px"};
-  margin: auto;
-  cursor: pointer;
-  opacity: 0.5;
-  z-index: 2;
-  color: black;
+width: 50px;
+height: 50px;
+background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),#fff7f7;
+border-radius: 50%;
+display: flex;
+align-items: center;
+justify-content: center;
+position: absolute;
+top: 0;
+bottom: 0;
+left: ${(props) => props.direction === "left" && "10px"};
+right: ${(props) => props.direction === "right" && "10px"};
+margin: auto;
+cursor: pointer;
+opacity: 0.5;
+z-index: 2;
+color: #1eb2a6;
+
+  &:hover{
+    background: #fff7f7;
+  }
 `;
 const Wrapper = styled.div`
   height: 100%;
   display: flex;
   transition: all 0.8s ease;
   transform: translateX(${(props) => props.slideIndex * -100}vw);
-`;
+  `;
 const Slide = styled.div`
   width: 100vw;
   height: 100vh;
