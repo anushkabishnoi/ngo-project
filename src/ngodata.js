679c5c41120672d2d01dadb203033f86a23ddc0e
Pragya Shah <pragyashah27@gmail.com>
2023-09-08 00:47:43 +0530

    making ngo project

---
+++ src/ngodata.js
@@ -0,0 +1,31 @@
+
+const items = [
+    { id: 1, name: 'Item 1', state: 'State A', content: 'Content for State A' },
+    { id: 2, name: 'Item 2', state: 'State B', content: 'Content for State B' },
+    { id: 3, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 4, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 5, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 6, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 7, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 8, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 9, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 10, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 11, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 12, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 13, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 14, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 15, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 16, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 17, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 18, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 19, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 20, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 21, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 22, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 23, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 24, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+    { id: 25, name: 'Item 3', state: 'State A', content: 'Content for State A' },
+  ];
+  
+  export default items;
+  
