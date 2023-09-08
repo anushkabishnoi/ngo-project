d88fba786107d100baa2ecbad95a198c4db1feac
anushkabishnoi <anushkabishnoi9@gmail.com>
2023-09-08 23:02:37 +0530

    contact us page updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,16 @@
-import React, { useRef, useState } from 'react';
-import emailjs from '@emailjs/browser';
-
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import "./Contact.css";
 
 const Result = () => {
   return (
-    <p>Your message has been successfully sent.</p>
-  )
-}
+    <div className="flex justify-center items-center">
+      <p className="text-highlight font-bold text-5xl">
+        Your message has been successfully sent!
+      </p>
+    </div>
+  );
+};
 
 export const Contact = () => {
   const form = useRef();
@@ -16,29 +19,39 @@ export const Contact = () => {
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('service_fved4dq', 'template_1qmk29z', form.current, '26Q2dDmAkcB0yghVS')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_fved4dq",
+        "template_1qmk29z",
+        form.current,
+        "26Q2dDmAkcB0yghVS"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+        },
+        (error) => {
           console.log(error.text);
-      });
-      form.current.reset();
-      showResult(true);
+        }
+      );
+    form.current.reset();
+    showResult(true);
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail}>
-      <label>Full Name</label>
-      <input type="text" name="fullName" />
-      <label>Phone Number</label>
-      <input type="text" name="phone" required />
-      <label>Enter Email</label>
-      <input type="email" name="email" required />
-      <label>Message</label>
-      <textarea className='message' name="message" required />
-      <input type="submit" value="Send" />
-      <div className="row">{result ? <Result/> : null}
-      </div>
-    </form>
+    <div className="form-body">
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Full Name</label>
+        <input type="text" name="fullName" />
+        <label>Phone Number</label>
+        <input type="text" name="phone" required />
+        <label>Enter Email</label>
+        <input type="email" name="email" required />
+        <label>Message</label>
+        <textarea className="message" name="message" required />
+        <input type="submit" value="Send" />
+        <div className="row">{result ? <Result /> : null}</div>
+      </form>
+    </div>
   );
 };
