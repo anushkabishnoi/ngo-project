ba37d0aa14c4ca9857b674446084e93f507f477d
Pragya Shah <pragyashah27@gmail.com>
2023-09-09 10:59:18 +0530

    Team section updated

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   }
 
   return (
-    <div className="container">
+    <div className="containerHeader">
       <Head />
       <header style={headerStyle}>
         <nav className="flexSB">
--- src/components/ourTeam/OurTeam.jsx
+++ src/components/ourTeam/OurTeam.jsx
@@ -1,94 +1,100 @@
+// OurTeam.js
+import React from "react";
 import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 import "./OurTeam.css";
 
 const OurTeam = () => {
   return (
-    <section className="section-team relative">
-      {/* <h1 className="ourteam">Our Team</h1> */}
-      <div className="heading-team  text-highlight text-extrabold text-7xl">
-        Our Team
-      </div>
+    <section className="section-white">
       <div className="team">
-        <div class="profile-card">
-          <div className="img">
-            <img src="./Images/anushka.jpg" alt="" />
-          </div>
-          <div className="caption">
-            <h3>Anushka Bishnoi</h3>
-            <p>Full Stack Developer</p>
-            <p>Learning Cyber Security</p>
-            <div className="social-links">
-              <a
-                href="https://github.com/anushkabishnoi"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <FaGithub />
-                </span>
-              </a>
-
-              <a
-                href="https://www.linkedin.com/in/pragya-shah-abb843221/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <FaLinkedin />
-                </span>
-              </a>
-
-              <a
-                href="https://twitter.com/PRAGYASHAH09"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <FaTwitter />
-                </span>
-              </a>
+        <div className="profile-card">
+          <div className="card-inner">
+            <div className="card-front">
+              <div className="img">
+                <img src="./Images/anushka.jpg" alt="" />
+              </div>
+            </div>
+            <div className="card-back">
+              <div className="caption">
+                <h3>Anushka Bishnoi</h3>
+                <p>Full Stack Developer</p>
+                <p>Learning Cyber Security</p>
+                <div className="social-links">
+                  <a
+                    href="https://github.com/anushkabishnoi"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="icon">
+                      <FaGithub />
+                    </span>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/pragya-shah-abb843221/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="icon">
+                      <FaLinkedin />
+                    </span>
+                  </a>
+                  <a
+                    href="https://twitter.com/PRAGYASHAH09"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="icon">
+                      <FaTwitter />
+                    </span>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
 
-        <div class="profile-card">
-          <div className="img">
-            <img src="./Images/pragya.jpeg" alt="" />
-          </div>
-          <div className="caption">
-            <h3>Pragya Shah</h3>
-            <p>Full Stack Developer</p>
-            <p>DevOps Developer</p>
-            <div className="social-links">
-              <a
-                href="https://github.com/Pragyashah09"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <FaGithub />
-                </span>
-              </a>
-
-              <a
-                href="https://www.linkedin.com/in/pragya-shah-abb843221/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <FaLinkedin />
-                </span>
-              </a>
-
-              <a
-                href="https://twitter.com/PRAGYASHAH09"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <FaTwitter />
-                </span>
-              </a>
+        <div className="profile-card">
+          <div className="card-inner">
+            <div className="card-front">
+              <div className="img">
+                <img src="./Images/pragya.jpeg" alt="" />
+              </div>
+            </div>
+            <div className="card-back">
+              <div className="caption">
+                <h3>Pragya Shah</h3>
+                <p>Full Stack Developer</p>
+                <p>DevOps Developer</p>
+                <div className="social-links">
+                  <a
+                    href="https://github.com/Pragyashah09"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="icon">
+                      <FaGithub />
+                    </span>
+                  </a>
+                  <a
+                    href="https://www.linkedin.com/in/pragya-shah-abb843221/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="icon">
+                      <FaLinkedin />
+                    </span>
+                  </a>
+                  <a
+                    href="https://twitter.com/PRAGYASHAH09"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <span className="icon">
+                      <FaTwitter />
+                    </span>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
