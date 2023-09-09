41c7ca28f61df9b664bd2f234949603b01c4e397
anushkabishnoi <anushkabishnoi9@gmail.com>
2023-09-09 13:13:21 +0530

    header 2

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Head from "./Head";
 import "./Header.css";
@@ -6,16 +6,19 @@ import "./Header.css";
 const Header = () => {
   const [click, setClick] = useState(false);
 
-  const headerStyle = {
-    width: "96vw",
-  };
+  const headerStyle={
+    width:"96vw",
+  }
 
   return (
     <div className="containerHeader">
       <Head />
       <header style={headerStyle}>
-        <nav className={`flexSB ${click ? "mobile-nav" : ""}`}>
-          <ul onClick={() => setClick(false)}>
+        <nav className="flexSB">
+          <ul
+            className={click ? "mobile-nav" : "flexSB "}
+            onClick={() => setClick(false)}
+          >
             <li>
               <Link to="/">Home</Link>
             </li>
@@ -25,6 +28,7 @@ const Header = () => {
             <li>
               <Link to="/about">About</Link>
             </li>
+
             <li>
               <Link to="/ourTeam">Team</Link>
             </li>
@@ -36,7 +40,11 @@ const Header = () => {
             <div className="button">GET STARTED</div>
           </div>
           <button className="toggle" onClick={() => setClick(!click)}>
-            {click ? <i className="fa fa-times"> </i> : <i className="fa fa-bars"></i>}
+            {click ? (
+              <i className="fa fa-times"> </i>
+            ) : (
+              <i className="fa fa-bars"></i>
+            )}
           </button>
         </nav>
       </header>
