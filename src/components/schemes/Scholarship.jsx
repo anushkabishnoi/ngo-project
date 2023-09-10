9e3826797cfe4fcee62de7dd8c4c5759f18f8a2e
anushkabishnoi <anushkabishnoi9@gmail.com>
2023-09-10 19:12:39 +0530

    scholarships 2

--- src/components/schemes/Scholarship.jsx
+++ src/components/schemes/Scholarship.jsx
@@ -1,8 +1,29 @@
 import Header from "../common/header/Header";
 import Footer from "../common/footer/Footer";
 import "./Scholarships.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import items from "../../ngodata";
 
 const Scholarship = () => {
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
     <div className="scholarships-body">
       <Header />
@@ -12,9 +33,25 @@ const Scholarship = () => {
             Explore Scholarship Opportunities
           </div>
         </div>
-        
+        <div className="carousel-heading text-highlght">
+          For Grade 5 and below
+        </div>
+        <Carousel
+          infinite={true}
+          className="carousel-parent"
+          responsive={responsive}
+        >
+          {items.map((item) => (
+            <div key={item.id} className="carousel-card">
+              <h1 className="text-5xl ">{item.name}</h1>
+              <a className="text-highlight" href={item.link}>
+                learn more
+              </a>
+            </div>
+          ))}
+        </Carousel>
       </section>
-      <Footer />
+      <Footer className="footer-scholarships" />
     </div>
   );
 };
