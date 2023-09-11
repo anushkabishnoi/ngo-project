a21053690c16c37c27aa6debe810c6b67174ba00
anushkabishnoi <anushkabishnoi9@gmail.com>
2023-09-11 08:38:00 +0530

    scholarships data formatted

--- src/scholarshipdata.js
+++ src/scholarshipdata.js
@@ -1,12 +1,44 @@
 const dataitems = [
-    { id: 1, name: 'Merit Based Scholarships', link: 'https://www.buddy4study.com/scholarships/merit-based-scholarship-in-india' },
-    { id: 2, name: 'State Based Scholarships', link: 'https://www.buddy4study.com/article/state-scholarships' },
-    { id: 3, name: 'Scholarships For Study Abroad', link: 'https://www.gyandhan.com/blogs/trending-scholarships-for-student-to-studying-abroad' },
-    { id: 4, name: 'Scholarships For Girls', link: 'https://www.buddy4study.com/article/scholarships-for-indian-girls-and-women' },
-    { id: 5, name: 'Minority Based Scholarships', link: 'https://www.minorityaffairs.gov.in/show_content.php?lang=1&level=2&ls_id=661&lid=823' },
-    { id: 6, name: 'Based On Family Income', link: 'https://www.buddy4study.com/article/scholarships-for-poor-students' },
-    { id: 7, name: 'Scholarships For Aspiring Athletes', link: 'https://www.buddy4study.com/article/scholarships-for-aspiring-athletes' },
-    { id: 8, name: 'School Scholarships', link: 'https://www.buddy4study.com/article/school-scholarships' },
-]
+  {
+    id: 1,
+    name: "Merit Based Scholarships",
+    link: "https://www.buddy4study.com/scholarships/merit-based-scholarship-in-india",
+  },
+  {
+    id: 2,
+    name: "State Based Scholarships",
+    link: "https://www.buddy4study.com/article/state-scholarships",
+  },
+  {
+    id: 3,
+    name: "Scholarships For Study Abroad",
+    link: "https://www.gyandhan.com/blogs/trending-scholarships-for-student-to-studying-abroad",
+  },
+  {
+    id: 4,
+    name: "Scholarships For Girls",
+    link: "https://www.buddy4study.com/article/scholarships-for-indian-girls-and-women",
+  },
+  {
+    id: 5,
+    name: "Minority Based Scholarships",
+    link: "https://www.minorityaffairs.gov.in/show_content.php?lang=1&level=2&ls_id=661&lid=823",
+  },
+  {
+    id: 6,
+    name: "Based On Family Income",
+    link: "https://www.buddy4study.com/article/scholarships-for-poor-students",
+  },
+  {
+    id: 7,
+    name: "Scholarships For Aspiring Athletes",
+    link: "https://www.buddy4study.com/article/scholarships-for-aspiring-athletes",
+  },
+  {
+    id: 8,
+    name: "School Scholarships",
+    link: "https://www.buddy4study.com/article/school-scholarships",
+  },
+];
 
 export default dataitems;
