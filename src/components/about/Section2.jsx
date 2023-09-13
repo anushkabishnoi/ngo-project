eb26821ce2d8e299e3cbb5d5ac499560d6aabaac
Pragya Shah <pragyashah27@gmail.com>
2023-09-13 21:51:28 +0530

    app

--- src/components/about/Section2.jsx
+++ src/components/about/Section2.jsx
@@ -1,63 +1,65 @@
 import Heading from "../common/heading/Heading";
+// import { homeAbout } from "../../data";
+// import Awrapper from "./Awrapper";
 import { FaTwitter, FaGithub } from "react-icons/fa";
 import { FaEnvelope } from "react-icons/fa";
 
 const aboutHomeStyle = {
+  // marginTop: "55%",
+  // height: "90vh",
   display: "grid",
-  gridTemplateColumns: "repeat(auto-fit, minmax(320px, 1fr))",
-  gap: "20px",
-  alignItems: "center",
-  paddingLeft: "10px",
-  paddingRight: "10px",
+  gridTemplateColumns: "repeat(2, 1fr)",
 };
-
 const imgContainerStyle = {
   background: "#E4E4D0",
-  borderRadius: "15px",
 };
-
 const imgStyle = {
-  height: "980px",
-  width: "100%",
+  width: "60%",
+  margin: "10%",
   borderRadius: "15px",
+  marginLeft: "30%",
 };
-
 const textStyle = {
-  marginBottom: "17%",
-  borderRadius: "15px",
+  // padding: "30%",/
+  marginBottom: "18%",
 };
 
 const footerStyle = {
-  marginTop: "6%",
+  marginTop: "20%", // Add margin to the top of the footer
 };
 
 const AboutCard = () => {
   return (
     <>
-      <section className="mt-t-50" style={aboutHomeStyle}>
+      <section className="mt-t-50 h-screen" style={aboutHomeStyle}>
         <div style={imgContainerStyle}>
-          <img
-            src="./Images/aboutCard.jpg"
-            style={imgStyle}
-            alt=""
-          />
+          <img src="./Images/aboutCard.jpg" style={imgStyle} alt="" />
         </div>
         <Heading
           style={textStyle}
           title="About Us"
           content="We are a diverse collective of individuals bound by a common passion for education and a shared belief in its transformative power. Our team is a mosaic of educators, developers, thinkers, and dreamers who have come together with a single purpose: to create a brighter future through accessible, inclusive education."
         />
+        {/* <div className="items">
+              {homeAbout.map((val) => {
+                return (
+                  <div className="item flexSB">
+                    <div className="img">
+                      <img src={val.cover} alt="" />
+                    </div>
+                    <div className="text">
+                      <h2>{val.title}</h2>
+                      <p>{val.desc}</p>
+                    </div>
+                  </div>
+                );
+              })}
+            </div> */}
       </section>
+      {/* <Awrapper /> */}
 
       <footer style={footerStyle}>
-        <ul
-          className="menuItems"
-          style={{
-            color: "black",
-            marginLeft: "10%",
-            paddingBottom: "2%",
-          }}
-        >
+        <ul className="menuItems" style={{ color: "black", marginLeft: "10%", paddingBottom: "2%", }}>
           <li className="menuItem">Terms Of Use</li>
           <li className="menuItem">About</li>
           <li className="menuItem">Blog</li>
@@ -69,7 +71,6 @@ const AboutCard = () => {
             href="https://github.com/anushkabishnoi/ngo-project"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ marginRight: "20px" }}
           >
             <span className="icon">
               <FaGithub />
@@ -80,7 +81,6 @@ const AboutCard = () => {
             href="mailto:protsahan.education@gmail.com"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ marginRight: "20px" }}
           >
             <span className="icon">
               <FaEnvelope />
@@ -91,7 +91,6 @@ const AboutCard = () => {
             href="https://twitter.com/PRAGYASHAH09"
             target="_blank"
             rel="noopener noreferrer"
-            style={{ marginRight: "20px" }}
           >
             <span className="icon">
               <FaTwitter />
@@ -99,17 +98,6 @@ const AboutCard = () => {
           </a>
         </div>
       </footer>
-
-      <style>
-        {`
-          @media (max-width: 768px) {
-            .desktop-header {
-              display: none;
-            }
-          }
-        `}
-      </style>
-
     </>
   );
 };
